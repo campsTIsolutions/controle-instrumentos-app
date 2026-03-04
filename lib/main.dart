7e83cb07300093c8d3298d17804a0a07b923f3c3
GuilhermeCamposGit-Hub <guicampos070@gmail.com>
2026-03-04 11:20:48 -0300

    login page, tela de cadastro e conexão com o supabase bem sucedida

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'features/login/login_page.dart';
 
-import 'core/config/supabase_config.dart';
-import 'app.dart';
-
-Future<void> main() async {
+void main() async {
+  // Garante a inicialização dos componentes do Flutter
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Inicializa a conexão real com o seu banco de dados Supabase
   await Supabase.initialize(
-    url: SupabaseConfig.url,
-    anonKey: SupabaseConfig.anonKey,
+    url: 'https://ylcfdbonhrvvbclinado.supabase.co',
+    anonKey: 'sb_publishable_YopOGM9CpLfvJXXiKNTHJw_Tx1RtZDn',
   );
 
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Controle de Instrumentos',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // Tema configurado para combinar com o seu layout Dark/Roxo
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF4D00FF),
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+      ),
+      home: const LoginPage(),
+    );
+  }
 }
