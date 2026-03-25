151f06d8daf169b1c89ebd75d0ac30cf22f60646
Murilo <104048357+murilo186@users.noreply.github.com>
2026-03-25 08:37:00 -0300

    Feature/route (#20)
    * Add navigation and logout flow
    
    * Fix drawer navigation context
    
    * Ajusta navegação no drawer e fundo da tela de login

--- lib/features/instrumentos/ui/widgets/app_drawer.dart
+++ lib/features/instrumentos/ui/widgets/app_drawer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:controle_instrumentos/features/alunos/alunos_page.dart';
-import '../../../login/login_page.dart';
+import 'package:controle_instrumentos/features/instrumentos/ui/instrumentos_page.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
@@ -8,45 +8,35 @@ class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      width: 200, 
+      width: 200,
       child: SafeArea(
-        child: Column(  
+        child: Column(
           children: [
             const SizedBox(height: 20),
             const Text(
               'Menu',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 5),
 
-            const Divider(
-              height: 1,
-              thickness: 2,
-            ),
+            const Divider(height: 1, thickness: 2),
 
             ListTile(
               leading: const Icon(Icons.music_note),
               title: const Text('Instrumentos'),
               onTap: () {
-                Navigator.pop(context);
+                final navigator = Navigator.of(context);
+                navigator.pop();
+                navigator.pushReplacement(
+                  MaterialPageRoute(builder: (_) => const InstrumentosPage()),
+                );
               },
             ),
 
             ListTile(
               leading: const Icon(Icons.article),
               title: const Text('Chamada'),
-              onTap: () {
-                final navigator = Navigator.of(context);
-                navigator.pop();
-                navigator.push(
-                  MaterialPageRoute(
-                    builder: (context) => const LoginPage(),
-                  ),
-                );
-              },
+              onTap: null,
             ),
 
             ListTile(
