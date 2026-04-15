f5532a09c033577ba52a23219161905e5ce98751
Murilo <murilofd_2006@hotmail.com>
2026-04-15 08:59:43 -0300

    chore(app): remove unused imports from app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,5 @@
 import 'package:controle_instrumentos/features/chamada/tela_anual.dart';
 import 'package:flutter/material.dart';
-import 'package:controle_instrumentos/features/instrumentos/ui/instrumentos_page.dart';
-import 'package:controle_instrumentos/features/login/login_page.dart';
-//import 'package:controle_instrumentos/lib/features/chamada/ui/presenca_page.dart';
-import 'package:controle_instrumentos/features/chamada/supabase_service.dart';
-import 'package:controle_instrumentos/features/chamada/models.dart';
-import 'package:controle_instrumentos/features/chamada/tela_atestado.dart';
-import 'package:controle_instrumentos/features/chamada/tela_dia.dart';
-import 'package:controle_instrumentos/features/chamada/tela_mes.dart';
-import 'package:controle_instrumentos/features/chamada/widgets.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
